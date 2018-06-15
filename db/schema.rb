ab08285dae716baa36dfc85036c2c95754004c9e
JamesAndresCM <jaimecanalesmarin@gmail.com>
2018-06-15 04:30:32 -0400

    cambio en el modelo de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615054817) do
+ActiveRecord::Schema.define(version: 20180615073254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(version: 20180615054817) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "slug"
+    t.index ["slug"], name: "index_universities_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
